fc042fc8969eb3d03f25d0dc6477b7bcb63f9944
sudebkumar <sudeb.phy137@gmail.com>
2014-07-09 14:02:31 +0600

    multidimentional

--- array.php
+++ array.php
@@ -14,6 +14,13 @@ print_r( $cars);
 echo "</pre>";
 //associative array
 $fruits = array('a'=>'apple','b'=>'banana');
+//$fruits[""] = 'this is last value;
+echo $fruits[""];
 echo "<pre>";
 print_r( $fruits );
 echo "</pre>";
+//echo $fruits['a'];
+$arr2D = array ($cars,$fruits);
+echo "<pre>";
+print_r($arr2D);
+echo "<pre>";
