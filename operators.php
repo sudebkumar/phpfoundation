b84f43ead5b9c9f85db98b9bf497ba218c2f1ae7
sudebkumar <sudeb.phy137@gmail.com>
2014-07-09 12:59:48 +0600

    changeoperator

--- operators.php
+++ operators.php
@@ -2,14 +2,28 @@
 
 $x = 100 / (2*5);
 $y = 100 % (2*5);
-$z =100 / 2*5 ;
+$z = 100 / 2*5 ;
 $a = 0/5;
 $b =0%5;
 
-echo $x."<br />";
-echo $y."<br />";
-echo $z."<br />";
-echo $a."<br />";
-echo $b."<br />";
+//echo $x."<br />";
+//echo $y."<br />";
+//echo $z."<br />";
+//echo $a."<br />";
+//echo $b."<br />";
+   
+$a = 3;
+$b = 5;
+if (3===$a)
+{
+//echo "Hi";
+}  
 
-?>
+$a =3;
+$b =5;
+if ($a=$b)
+{
+
+
+echo "Hello";
+} 
