689368d2d16713029ccadbfacfee90f6ecfb862f
sudebkumar <sudeb.phy137@gmail.com>
2014-07-09 11:24:21 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+//echo "Hello world";
+
+
+//syntax Exercise 
+
+//variables
+$a =1;
+//block
+//Nested block
+{
+
+	
+	
+	{
+	
+		}
+}
+
+
+//Data Type 
+
+$_aNumber = '1234';
+//echo getype ($_aNumber);
+//echo gettype ($aNumber, 'String');
+//echo gettype ($anumber);
+
+$_aString = abcd;
+$_aString ='abcd$aNumber';
+$_anotherString = "abcd$aNumber";
+
+//echo gettype ($_aString);
+//echo gettype ($_anotherString);
+
+
+$aFloateNumber = 12.12;
+//echo gettype ($aFloateNumber);
+
+$aBool = true;
+$anotherBool= false;
+//echo gettype ($aBool);
+//echo "<br>";
+//echo gettype ($anotherBool);
+
+$a = 2*3+5;
+//echo $a."\n";
+//echo $a;
+?>
+<?php
+$name='sudeb'; 
+for ($i=1;$i<=5;$i++)
+{
+
+?
+?
+?>
+
+?>
