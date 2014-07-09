e3c68e97a37c738f5d9aad4308b340f815b81273
sudebkumar <sudeb.phy137@gmail.com>
2014-07-09 15:37:09 +0600

    foreach

---
+++ foreach.php
@@ -0,0 +1,7 @@
+<?php 
+$cars =array ('toy', 'sed', 'mercc');
+$fruits = array ("t"=> 'test','a'=>'apple', 'b'=>'banana');
+for ($i =0; $i <count ($cars); $i++)
+{
+echo $cars[$i]."<br/>";
+}
---
+++ loops.php
@@ -0,0 +1,10 @@
+<?php
+$start=1;
+$end= 10;
+for($start >=1;$end<=10;)
+{
+sum= $sum+1;
+echo "sum";
+break o;
+}
+?>
