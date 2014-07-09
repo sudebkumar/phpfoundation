472263b4ef9365b24307d4c41fe86f784ba10aae
sudebkumar <sudeb.phy137@gmail.com>
2014-07-09 13:29:37 +0600

    associated

--- array.php
+++ array.php
@@ -4,8 +4,16 @@
 $cars = array ('toyota','sedan','mercidi');
 //echo gettype ($cars);
 
+$cars[8] = 'suzuki';
+$cars [] ='corola';
+$cars [c]= 'ford';
+$cars [] = 'ferri';
 echo"<pre>";
-
 print_r( $cars);
 
+echo "</pre>";
+//associative array
+$fruits = array('a'=>'apple','b'=>'banana');
+echo "<pre>";
+print_r( $fruits );
 echo "</pre>";
