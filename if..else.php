3fb8d5fea0e656adc69240cb20ceed27891d2281
sudebkumar <sudeb.phy137@gmail.com>
2014-07-09 14:41:28 +0600

    again

--- if..else.php
+++ if..else.php
@@ -1,32 +1,32 @@
 <?php 
-$marks=90;
+$marks=37;
 
 
-if($marks<=32)
+if($marks>=80)
 {
-echo "$grade=F";
+echo "$grade=A+";
 }
-else if($marks>=33 && $marks <=39)
-{
-echo "$grade=D";
-}
-else if($marks>=40 && $marks <=49)
-{
-echo "$grade=C";
-}
-else if($marks>=50 && $marks <=59)
-{
-echo "$grade=B";
-}
-else if($marks>=60 && $marks <=69)
-{
-echo "$grade=A-";
-}
-else if($marks>=70 && $marks <=79)
+else if($marks>=70)
 {
 echo "$grade=A";
 }
-else if($marks>=80 && $marks <=99)
+else if($marks>=60)
 {
-echo "$grade=A+";
+echo "$grade=A-";
+}
+else if($marks>=50)
+{
+echo "$grade=B";
+}
+else if($marks>=40)
+{
+echo "$grade=C";
+}
+else if($marks>=33)
+{
+echo "$grade=D";
+}
+else if($marks<=32)
+{
+echo "$grade=F";
 }
