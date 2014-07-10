24e52850dd2cdd966040d4a919509f6f45aceca0
sudebkumar <sudeb.phy137@gmail.com>
2014-07-10 12:47:07 +0600

    define and acces of class method

--- opclass.php
+++ opclass.php
@@ -3,10 +3,15 @@
 class User{
   var $name ="FTFL";
   var $email ="";
+  
+  function getName() {
+  return "FTFL2";
+  }
 }
 
 //instantiate an object from a class 
 $user = new User();
 //var_dump ($user);
 echo $user-> name;
+echo $user->getName();
 ?>
