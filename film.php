95c25639c1f8fefe40f859409a9e76e093e596f1
sudebkumar <sudeb.phy137@gmail.com>
2014-07-10 11:10:31 +0600

    film

---
+++ film.php
@@ -0,0 +1,12 @@
+<?php 
+$films =array(
+        "genres" => array( "comedy","tragedy","action","romantic"),
+		"film-titles" => array( "Big", "Star Wars", "Titanic","French Kiss"),
+		"stars" => array ("Bill Murrey","Merk Hammell"."Leonard Dicaprio","Cate Blanchell"),);
+		
+		echo "<pre>";
+		print_r ($films);
+		echo "</pre>";
+		
+	
+		
